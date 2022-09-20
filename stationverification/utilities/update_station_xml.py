868b396878238dc27f0ea6a262e585cf6c04b49a
Hasan Issa <hasan.issa@nrcan-rncan.gc.ca>
2022-09-20 13:55:01 -0400

    Fetch station xml changes

--- stationverification/utilities/update_station_xml.py
+++ stationverification/utilities/update_station_xml.py
@@ -9,7 +9,7 @@ def update_station_xml():
     if not os.path.isfile("stationverification/data/QW.xml"):
         open('stationverification/data/QW.xml', 'wb')
 
-    station_url = "https://earthquakescanada.nrcan.gc.ca/fdsnws/station/1/builder"  # noqa
+    station_url = "https://earthquakescanada.nrcan.gc.ca/fdsnws/station/1/query?network=QW&level=response&nodata=404"  # noqa
     request = requests.get(station_url, allow_redirects=True)
     request.raise_for_status()
     open('stationverification/data/QW.xml', 'wb').write(request.content)
